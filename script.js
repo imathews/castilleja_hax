2f65dc2ebbf0642bf9f065f8e2c3bafc17e55931
Elizabeth Ledwith <matthewledwith@Elizabeth-Ledwiths-MacBook-Air.local>
2015-10-10 16:02:40 -0700

    added the ability to delete links

--- script.js
+++ script.js
@@ -45,7 +45,25 @@ castiHax.initTeam('TeamEKM', function(){
 			for (i = 0; i < res.length; i++) { 
     			var name = res[i][0]
     			var link = res[i][1]
-    			document.getElementById('results').innerHTML += "<div>" + name + " - <a href='" + link + "'>" + link + "</a><a>Delete</a></div>";
+    			document.getElementById('results').innerHTML += "<div class='row'><div class='col-sm-3'>" + name + " - <a href='" + link + "'>" + link + "</a></div><div class='col-sm-4'><a class='del' id='" + i + "'>Delete</a></div></div>";
+    			$('.del').on('click', function(object){
+    				console.log("delete")
+    				console.log(object.target.id)
+    				var index = object.target.id
+
+					castiHax.get(category, function(res){
+						request = {}
+						var vals = []
+						if (res !== null) {
+						  vals = res;
+						} 
+						vals.splice(parseInt(index), 1);
+						console.log(vals)
+						request[category] = vals;
+						castiHax.set(request);
+						alert("Congratulations! You have sucessfully deleted a link");
+					});
+				});
 			}
 		});
 
