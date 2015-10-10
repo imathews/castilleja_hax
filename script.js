a1e6b8427f9064d0f7d31d0722240fdb189d005d
Kempton <20kmccarty@castilleja.org>
2015-10-10 14:21:34 -0700

    now link.html lets you save links to database

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-castiHax.initTeam('Ian&Sierra', function(){
+castiHax.initTeam('TeamEKM', function(){
 
-	castiHax.set({hello: 'worldyyy'}, function(err){
+	// castiHax.set({hello: 'worldyyy'}, function(err){
+
+	// });
+
+	// castiHax.get("Test Category", function(err){
+	// 	alert(err);
+	// });
+
+	$('#submitNewLink').on('click', function(){
+		var category =  $('#catagory').val();
+		var linkName = $('#link-name').val();
+		var link = $('#link').val();
+		console.log(category);
+		console.log(linkName);
+		console.log(link);
+
+		castiHax.get(category, function(res){
+			request = {}
+			var vals = []
+			if (res !== null) {
+			  alert(res);
+			  vals = res;
+			} 
+			vals.push([linkName, link]);
+			request[category] = vals;
+			castiHax.set(request);
+		});
+
 
 	});
-	$('#submit').on('click', function(){
-		console.log($('#email').val());
-		castiHax.set({email: $('#email').val()});
-	})
 
 });
 
